12bb913dc679b02bfd39ed7c9e7e1409551ee5dc
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-03-13 15:46:27 -0600

    Hotfix unit

--- utils/unit_matcher.py
+++ utils/unit_matcher.py
@@ -1,13 +1,16 @@
 def convert_cpu_unit(size):
     # Returns the milicores
     # If no units follow the number, assume it is a single core
-    if str(size)[-1].isdigit():
+    size = str(size)
+    if size[-1].isdigit():
         return int(size) * 1000
     # If the unit is "m", assume it is milicores
-    elif str(size)[-1] == "m":
+    elif size[-1] == "m":
         return int(size[:-1])
 
 def convert_memory_unit(size):
+    size = str(size)
+
     if size[-2:] == "Ki":
         return int(size[:-2])
     elif size[-2:] == "Mi":
