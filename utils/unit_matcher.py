32321ad3b32a62743c20ac3a37fd38af2336abd8
Seonwoo Kim <88951594+seki5405@users.noreply.github.com>
2023-03-13 21:47:43 -0600

    Fix n unit

--- utils/unit_matcher.py
+++ utils/unit_matcher.py
@@ -3,21 +3,21 @@ def convert_cpu_unit(size):
     # If no units follow the number, assume it is a single core
     size = str(size)
     if size[-1].isdigit():
-        return float(size) * 1000
+        return round(float(size) * 1000, 3)
     # If the unit is "m", assume it is milicores
     elif size[-1] == "m":
-        return float(size[:-1])
+        return round(float(size[:-1]), 3)
     elif size[-1] == "n":
-        return float(size[:-1]) / 1000
+        return round(float(size[:-1]) / 1000 / 1000, 3)
 
 def convert_memory_unit(size):
     size = str(size)
 
     if size[-2:] == "Ki":
-        return float(size[:-2])
+        return round(float(size[:-2]), 3)
     elif size[-2:] == "Mi":
-        return float(size[:-2]) * 1024
+        return round(float(size[:-2]) * 1024, 3)
     elif size[-2:] == "Gi":
-        return float(size[:-2]) * 1024 * 1024
+        return round(float(size[:-2]) * 1024 * 1024, 3)
     else:
         raise Exception("Invalid unit")
